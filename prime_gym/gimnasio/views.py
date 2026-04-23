75e4031ece8950c348deedb520744b485a3ffdb3
cosmincostea21 <cosmincostea0810@gmail.com>
2026-04-23 17:40:03 +0200

    rama-cosmin/insertar-templates

--- prime_gym/gimnasio/views.py
+++ prime_gym/gimnasio/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    return render(request,"gimnasio/index.html")
+
+def clases(request):
+    return render(request,"gimnasio/class-details.html")
+
+def tarifas(request):
+    return render(request,"gimnasio/services.html")
+
+def equipo(request):
+    return render(request,"gimnasio/team.html")
+
+def contacto(request):
+    return render(request,"gimnasio/contact.html")
