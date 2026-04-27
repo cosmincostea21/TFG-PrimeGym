4c812f92284cba1d42caa13c8a648c17189a2890
cosmincostea21 <cosmincostea0810@gmail.com>
2026-04-27 12:08:43 +0200

    {rama-cosmin} urls - componentes

--- prime_gym/perfil/urls.py
+++ prime_gym/perfil/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'perfil'
+
 urlpatterns = [
-    path('',views),
-    path('datos/', views),
-    path('reservas/', views),
-    path('tarifa/', views),
+    path('', views.dashboard, name='dashboard'),
+    path('datos/', views.datos_personales, name='datos_personales'),
+    path('reservas/', views.mis_reservas, name='mis_reservas'),
+    path('clases/', views.clases_disponibles, name='clases_disponibles'),
+    path('tarifa/', views.cambiar_tarifa, name='cambiar_tarifa'),
 ]
+
