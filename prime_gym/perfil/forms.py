e93b01ce75705f2c6ac3592a6d356ca9ba26fb26
cosmincostea21 <cosmincostea0810@gmail.com>
2026-05-14 13:41:32 +0200

    {cosmin} evitar repeticion de email

--- prime_gym/perfil/forms.py
+++ prime_gym/perfil/forms.py
@@ -16,6 +16,10 @@ class UserUpdateForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este email ya está registrado.")
+
         try:
             validate_email(email)
         except ValidationError:
